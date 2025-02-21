2ddee19d00f46d36017a4c7ba5e8152ea08c6d8f
arekGuba <150472178+arekGuba@users.noreply.github.com>
2025-02-21 15:11:38 -0600

    TreeProblems.java
    yuuuuup

--- TreeProblems.java
+++ TreeProblems.java
@@ -1,5 +1,5 @@
 /*
- * *** YOUR NAME GOES HERE / YOUR SECTION NUMBER ***
+ * *** Arek Gubala / COMP 272-001 ***
  *
  * This java file contains several simple tree problems that need to be
  * codified. These routines  must use the TreeMap and TreeSet library
@@ -7,58 +7,94 @@
  *
  */
 
-import java.util.*;
+ import java.util.*;
 
-public class TreeProblems {
+ public class TreeProblems {
+ 
+   /**
+    * Method different()
+    *
+    * Given two TreeSets of integers, return a TreeSet containing all elements
+    * that are NOT in both sets. In other words, return a TreeSet of all the
+    * elements that are in one set but not the other.
+    */
 
-  /**
-   * Method different()
-   *
-   * Given two TreeSets of integers, return a TreeSet containing all elements 
-   * that are NOT in both sets. In other words, return a TreeSet of all the
-   * elements that are in one set but not the other.
-   */
-  
-  public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
+   public static Set<Integer> different(Set<Integer> setA, Set<Integer> setB) {
 
-    // INSERT CODE HERE - DO NOT FORGET TO PLACE YOUR NAME ABOVE
-    //
-    // This can be done numerous ways, but once such will only that
-    // *several* lines of code. Hint: create two temporary TreeSets and utilize the
-    // methods retainAll(), addAll(), and removeAll(). But in the end, get something to work.
+     // INSERT CODE HERE - DO NOT FORGET TO PLACE YOUR NAME ABOVE
+     //
+     // This can be done numerous ways, but once such will only that
+     // *several* lines of code. Hint: create two temporary TreeSets and utilize the
+     // methods retainAll(), addAll(), and removeAll(). But in the end, get something
+     // to work.
 
-    return setA;
-  }
+     Set<Integer> union = new HashSet<>(setA);
+     Set<Integer> intersection = new HashSet<>(setA);
 
+     union.addAll(setB);  // every individual element in sets A and B combined
+     intersection.retainAll(setB); // every element that exists in both set A and B
 
-  /**
-   * Method removeEven()
-   *
-   * Given a treeMap with the key as an integer, and the value as a String,
-   * remove all <key, value> pairs where the key is even. 
-   */
+     // Every element - elements that exist in both sets = elements that ONLY exists EITHER in A or B
+     union.removeAll(intersection);
 
-  public static void removeEven(Map<Integer, String> treeMap) {
+     return union;
+   }
+ 
+   /**
+    * Method removeEven()
+    *
+    * Given a treeMap with the key as an integer, and the value as a String,
+    * remove all <key, value> pairs where the key is even. 
+    */
+ 
+   public static void removeEven(Map<Integer, String> treeMap) {
 
-    // INSERT CODE HERE.
+     // INSERT CODE HERE.
 
-    return;
-  }
+    /*
+    * to prevent an error occuring as we delete keys while we traverse the set of treeMap's keys,
+    * first, we make a temporary set to store the even keys of treeMap's keys. once we have
+    * them, delete elements with those keys in treeMap
+    */
+    
+    // empty set which will contain the even keys in treeMap
+     Set<Integer> evenKeys = new HashSet<>();
 
+     // for-each key in treeMap, if it's even, add that key integer to evenKeys
+     for (Integer i : treeMap.keySet()) {
+        if (i % 2 == 0){
+          evenKeys.add(i);
+        }
+     }
+     // now that we have all even keys, remove them from treeMap
+     for (Integer i : evenKeys){
+        treeMap.remove(i);
+     }
+     return;
+   }
+ 
+ 
+   /**
+    * Method treesEqual()
+    *
+    * Given two treeMaps, each with the key as an integer, and the value as a String,
+    * return a boolean value indicating if the two trees are equal or not.
+    */
+ 
+   public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2 ) {
+ 
+     // INSERT CODE HERE
 
-  /**
-   * Method treesEqual()
-   *
-   * Given two treeMaps, each with the key as an integer, and the value as a String,
-   * return a boolean value indicating if the two trees are equal or not.
-   */
+     // if both trees are null, they they're equal to each other
+     if (tree1 == null && tree2 == null){
+      return true;
+    }
+    // no point in checking if treeMaps are equal if only one is null
+    if (tree1 == null || tree2 == null){
+      return false;
+    }
 
-  public boolean treesEqual(Map<Integer, String> tree1,Map<Integer, String> tree2 ) {
-
-    // INSERT CODE HERE
-
-    return false;
-
-  }
-
-} // end treeProblems class
+    return (tree1.equals(tree2));
+   }
+ 
+ } // end treeProblems class
